80562a4b6643d75cf634a24c6489c666d95ccfef
sara pereira <goncalvesarag@gmail.com>
2019-06-19 16:19:23 -0300

    Tentando Solucionar Problemas

--- Octopus.py
+++ Octopus.py
@@ -3,21 +3,28 @@ from chatterbot import ChatBot #Construtor, para criar o programa
 
 
 bot = ChatBot(
-    'Octopus Chat Bot',
+    'Octopus',
     trainer='chatterbot.trainers.ListTrainer'
 )
 
-conversa = ['Oi','Olá','Tudo bem?','Tudo ótimo','Você gosta de programar?','Sim, eu programo em Python','Qual seu nome?','Meu nome é Octopus, eu sou um Polvo','Quantos anos você tem?','Eu tenho oito patinhas']
+conversa = ['Oi','Olá','Tudo bem?',
+            'Tudo ótimo','Você gosta de programar?',
+            'Sim, eu programo em Python','Qual seu nome?',
+            'Meu nome é Octopus, eu sou um Polvo',
+            'Quantos anos você tem?','Eu tenho oito patinhas']
 
-bot.set_trainer(ListTrainer)
-bot.train(conversa)
+# bot.set_trainer(ListTrainer)
+# bot.train(conversa)
 
-print('\033[35mOCTOPUS CHATBOT\033[m')
+trainer = ListTrainer(bot)
+trainer.train(conversa)
+
+print('\033[35mOCTOPUS\033[m')
 print('\033[31m ___\033[m')
 while True: #Loop
 
     pergunta = input('\033[32mUsuário: \033[m')#Entrada do usuário
-    print('\033[34m--- \033[m')
+    print('\033[34m___ \033[m')
     resposta = bot.get_response(pergunta)#Resposta do bot
 
     if float(resposta.confidence) > 0.5: #Grau de confiança da resposta
