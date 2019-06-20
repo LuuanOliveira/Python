d5e8ec7f9b01690ecefed3a7c22d922806215cf9
Victor Luan <victorluan17@hotmail.com>
2019-06-20 15:58:41 -0300

    Add Sugestões do User na Conversa

--- Octopus.py
+++ Octopus.py
@@ -1,27 +1,38 @@
 from chatterbot.trainers import ListTrainer #Permite o uso de lista de strings
 from chatterbot import ChatBot #Construtor, para criar o programa
 
-
 bot = ChatBot(
-    'Octopus Chat Bot',
+    'Octopus',
     trainer='chatterbot.trainers.ListTrainer'
 )
 
-conversa = ['Oi','Olá','Tudo bem?','Tudo ótimo','Você gosta de programar?','Sim, eu programo em Python','Qual seu nome?','Meu nome é Octopus, eu sou um Polvo','Quantos anos você tem?','Eu tenho oito patinhas']
+conversa = ['Oi','Olá','Tudo bem?', #Lista de treinamento do boot
+            'Tudo ótimo','Você gosta de programar?',
+            'Sim, eu programo em Python','Qual seu nome?',
+            'Meu nome é Octopus, eu sou um Polvo',
+            'Quantos anos você tem?','Eu tenho oito patinhas']
 
-bot.set_trainer(ListTrainer)
-bot.train(conversa)
+trainer = ListTrainer(bot)
+trainer.train(conversa)
 
-print('\033[35mOCTOPUS CHATBOT\033[m')
-print('\033[31m ___\033[m')
-while True: #Loop
-
-    pergunta = input('\033[32mUsuário: \033[m')#Entrada do usuário
-    print('\033[34m--- \033[m')
+print('OCTOPUS')
+print('___')
+while True:#Loop enquanto for verdadeiro faça
+    pergunta = input('EU: ')#Entrada do usuário
+    print('___')
     resposta = bot.get_response(pergunta)#Resposta do bot
 
     if float(resposta.confidence) > 0.5: #Grau de confiança da resposta
-        print('\033[36mOctopus: \033[m', resposta)
-        print('\033[31m ___\033[m')
+        print('OCTUPUS: ', resposta)#Resposta do bot
+        print('___')
+
+    #elif float(resposta.confidence) < 0.5:  #Grau de confiança da resposta
+
+
     else:
-        print('\033[36mOctopus: \033[m','\033[31mAinda não sei responder está pergunta\033[m')
+        print('OCTUPUS: Ainda não sei responder está pergunta')
+        conversa.append(str(input('Repita a Pergunta: ')))#Entrada do usuário adicionando na lista
+        conversa.append(str(input('Como você Responderia: ')))#Entrada do usuário adicionando na lista
+        print(conversa)
+        print('Aprendi, Obrigado!')
+        print('___')
